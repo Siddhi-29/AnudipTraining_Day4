f6b88aef5dbce85b21d20535bcb33c18f4a510e0
siddh <siddh@Siddhi>
2022-09-08 18:10:28 +0530

    This is an assignment

---
+++ GeometricalShapes/src/Square.java
@@ -0,0 +1,16 @@
+
+public class Square extends Shape {
+	double side ;
+	Square(double s){
+		side = s;
+	}
+	@Override
+	void getArea() {
+		System.out.println("Area = "+ (this.side*this.side));
+	}
+
+	@Override
+	void getShape() {
+		System.out.println("This is a Square");
+	}
+}
